44fd3efebfa73acce3afa336380b5fa3a73bc1ec
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-05-27 10:41:43 +0530

    Argument parser

--- argument.v
+++ argument.v
@@ -10,19 +10,36 @@ struct ArgumentParserResults {
 	parameters map[string]string
 }
 
-fn create_argument_parser(arguments []string) int {
+fn find_parameter_value(statement []string) (string, string) {
+	command := statement[0]
+	if statement.len == 1 {
+		return command, default_flag_value
+	}
+	join_array := statement[1..]
+	return command, join_array.join('=')
+}
+
+fn create_argument_parser(arguments []string) ArgumentParserResults {
 	mut results := ArgumentParserResults{}
 	mut argument_index := 0
 	for argument in arguments {
+
 		if argument_index == 0 {
+			argument_index += 1
 			results.command = argument
+			continue
 		}
 		is_valid_command := argument.starts_with('--')
 		if !is_valid_command {
-			
+			Exception{
+				error : '$argument is not a valid flag'
+			}.create(true)
 		}
+		statement := argument.split('=')
+		command, params := find_parameter_value(statement)
 
+		results.parameters[command] = params
 		argument_index += 1
 	}
-	return 0
+	return results
 }
--- exception.v
+++ exception.v
@@ -9,13 +9,14 @@ struct Exception {
 fn (exception Exception) create(is_fatal bool) {
 	println('ERROR:${exception.error}')
 	mut suggestion_string := ''
-	if exception.file {
+	if exception.file.len != 0 {
 		suggestion_string += 'In ${exception.file} '
 	}
 
-	if exception.line_number {
+	if exception.line_number > 0 {
 		suggestion_string += "at ${exception.line_number}"
 	}
+	println(suggestion_string)
 
 	if is_fatal {
 		exit(1)
